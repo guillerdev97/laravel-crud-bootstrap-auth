3943120ece6dc7c5310df8f0e54d86cc14e13dfe
Guillermo García <guillermieres97@gmail.com>
2022-08-22 11:41:47 +0200

    display data on welcome view

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -17,9 +17,9 @@ class EventController extends Controller
         //
         $events = Event::get();
         /* $events = "Hello"; */
-        var_dump($events); 
+        /* var_dump($events);  */
 
-      /*  return view('welcome', compact('events')); */
+       return view('welcome', compact('events'));
     }
 
     /**
